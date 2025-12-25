a8ce95cc16691886f3f257d9ed576f752ebe9c9b
Ilya <steamistl@gmail.com>
2025-12-25 16:12:05 +0400

    добавил products

--- src/products/products.storage.js
+++ src/products/products.storage.js
@@ -7,11 +7,11 @@ exports.getById = (id) => read("products.json").find((p) => p.id === id);
 exports.updateById = (id, updates) => {
   const products = read("products.json");
   const index = products.findIndex((p) => p.id === id);
-  
+
   if (index === -1) {
     return null;
   }
-  
+
   products[index] = { ...products[index], ...updates };
   write("products.json", products);
   return products[index];
