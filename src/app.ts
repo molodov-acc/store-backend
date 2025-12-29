15005906501b9af6320fb2679e44222c4c16541c
Ilya <steamistl@gmail.com>
2025-12-29 18:54:41 +0400

    внес изменения в корс

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { router as productsRoutes } from "./products/products.routes";
 import { router as authRoutes } from "./auth/auth.routes";
 import { router as cartRoutes } from "./cart/cart.routes";
@@ -9,7 +10,14 @@ import { errorMiddleware } from "./middlewares/error";
 export function createApp() {
   const app = express();
 
+  app.use(
+    cors({
+      origin: "http://localhost:5174",
+      credentials: true,
+    })
+  );
   app.use(express.json());
+  app.use(express.static("public"));
 
   app.use("/auth", authRoutes);
   app.use("/products", productsRoutes);
--- src/products/types.ts
+++ src/products/types.ts
@@ -1,7 +1,10 @@
 export interface ProductQuery {
   gender?: string;
+  category?: string;
   size?: string;
   categoryId?: string;
   brandId?: string;
+  title?: string;
+  image?: string;
   color?: string;
 }
